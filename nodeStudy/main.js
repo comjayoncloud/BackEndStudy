f621536857d455a50e566ce65d390a13e17eaf08
jaewoo <comjayoncloud@gmail.com>
2022-06-08 16:11:16 +0900

    동기 비동기 간단예제

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -1,7 +1,17 @@
-const cowsay = require("cowsay");
+const fs = require("fs"); // 코어 모듈
 
-console.log(
-  cowsay.say({
-    text: "I love javascript",
-  })
-);
+// console.log("start");
+
+// let content = fs.readFileSync("./new", "utf8");
+
+// console.log(content);
+
+// console.log("finish");
+
+console.log("start");
+
+fs.readFile("./new", "utf8", (error, data) => {
+  console.log(data);
+});
+
+console.log("finish");
