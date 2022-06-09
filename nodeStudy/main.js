7e10f52790083176e0d344206e192a2da4e12dca
jaewoo <comjayoncloud@gmail.com>
2022-06-09 15:02:49 +0900

    EventEmitter 예제4

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -1,17 +1,12 @@
 const EventEmitter = require("events"); // 코어모듈
 
 const myEmitter = new EventEmitter(); // 객체로 만들어줘야 사용이 가능
-const myEmitter2 = new EventEmitter();
 
-//이벤트를 발생시키고 그 이벤트에대한 콜백이 실행되는 현상은 하나의 이벤트 에미터 객체 내에서만 이뤄진다
-myEmitter.on("test", () => {
-  console.log("1!");
-});
-myEmitter.on("test", () => {
-  console.log("2!");
-});
-myEmitter2.on("test", () => {
-  console.log("3!");
+myEmitter.on("test", (arg1, arg2, arg3, arg4) => {
+  console.log(arg1);
+  console.log(arg2);
+  console.log(arg3);
+  console.log(arg4);
 });
 
-myEmitter.emit("test");
+myEmitter.emit("test", "apple", "banana", "pear");
