bf26230fae7c44489c71fc845f3f8925833d9d67
jaewoo <comjayoncloud@gmail.com>
2022-06-09 15:05:10 +0900

    EventEmitter 예제5

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -2,11 +2,10 @@ const EventEmitter = require("events"); // 코어모듈
 
 const myEmitter = new EventEmitter(); // 객체로 만들어줘야 사용이 가능
 
-myEmitter.on("test", (arg1, arg2, arg3, arg4) => {
-  console.log(arg1);
-  console.log(arg2);
-  console.log(arg3);
-  console.log(arg4);
+const obj = { type: "text", data: "hello codeit", date: "2020-09-01" };
+
+myEmitter.on("test", (info) => {
+  console.log(info);
 });
 
-myEmitter.emit("test", "apple", "banana", "pear");
+myEmitter.emit("test", obj);
