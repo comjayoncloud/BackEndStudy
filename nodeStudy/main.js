a2546f0e62cadeeddb0f10f11efe806f05497863
jaewoo <comjayoncloud@gmail.com>
2022-06-11 14:05:07 +0900

    라우팅 예제2

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -7,6 +7,12 @@ let server = http.createServer(function (request, response) {
     response.end("<h1>Weclome</h1>");
   } else if (request.url === "/users") {
     response.end("<h1>" + users + "</h1>");
+  } else if (request.url.split("/")[1] === "users") {
+    //url : /users/1,/users/2, ..
+    let userIdx = request.url.split("/")[2];
+    let userName = users[userIdx - 1];
+
+    response.end("<h1>" + userName + "</h1>");
   } else {
     response.end("<h1>Page not available</h1>");
   }
