8dae49645d153a7a2a14d00fd12ad2c3a62773f8
jaewoo <comjayoncloud@gmail.com>
2022-06-11 14:30:02 +0900

    라우팅 예제3 코드 간소화

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -1,20 +1,21 @@
-const http = require("http");
+const http = require("http"); // etc
 
-users = ["tom", "andy", "jessica", "paul"];
+const users = ["Tom", "Andy", "Jessica", "Paul"]; // const
 
-let server = http.createServer(function (request, response) {
+const server = http.createServer((request, response) => {
+  // Arrow Function, const
   if (request.url === "/") {
-    response.end("<h1>Weclome</h1>");
+    response.end("<h1>Welcome!</h1>");
   } else if (request.url === "/users") {
-    response.end("<h1>" + users + "</h1>");
+    response.end(`<h1>${users}/h1>`); // Template String
   } else if (request.url.split("/")[1] === "users") {
-    //url : /users/1,/users/2, ..
-    let userIdx = request.url.split("/")[2];
-    let userName = users[userIdx - 1];
+    // url : /users/1, /users/2, .. // etc
+    const userIdx = request.url.split("/")[2];
+    const userName = users[userIdx - 1]; // etc
 
-    response.end("<h1>" + userName + "</h1>");
+    response.end(`<h1>${userName}</h1>`); // Template String
   } else {
-    response.end("<h1>Page not available</h1>");
+    response.end("<h1>Page Not Available</h1>");
   }
 });
 
