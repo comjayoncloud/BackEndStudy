53bc79c820eb58a5d353b656ab4d7b8e231bc094
jaewoo <comjayoncloud@gmail.com>
2022-06-13 22:59:11 +0900

    express basic 예제

---
+++ expressJs/cowork/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express(); // 관습적으로 app
+
+app.get("/hello", (req, res) => {
+  res.send("<h1>Hello Express</h1>");
+}); // 콜백 혹은 route handler라고함
+// req : 이 객체를 통해서 클라이언트가 보낸 request를 다룰수 있음
+// res : 이 객체를 통해서 적절한 리스폰스를 보낼수 있다.
+
+app.listen(3000);
