e0762e32df52649c8e7b1da916ceaf27fd377905
jaewoo <comjayoncloud@gmail.com>
2022-06-14 14:23:11 +0900

    db조회 예제2

--- expressJs/cowork/app.js
+++ expressJs/cowork/app.js
@@ -10,6 +10,17 @@ app.get("/api/members", (req, res) => {
 // req : 이 객체를 통해서 클라이언트가 보낸 request를 다룰수 있음
 // res : 이 객체를 통해서 적절한 리스폰스를 보낼수 있다.
 
+app.get("/api/members/:id", (req, res) => {
+  // const id = req.params.id;
+  const { id } = req.params;
+  const member = members.find((m) => m.id === Number(id));
+  if (member) {
+    res.send(member);
+  } else {
+    res.status(404).send({ message: "there is no such member" });
+  }
+}); // :id는 라우트 파라미터
+
 app.listen(3000, () => {
   console.log("server is listening..");
 });
