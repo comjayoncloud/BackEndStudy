053db682930ad979dd123eb29c169df9ae503f23
jaewoo <comjayoncloud@gmail.com>
2022-06-16 23:31:28 +0900

    특정 데이터 조회 예제

--- expressJs/cowork/app.js
+++ expressJs/cowork/app.js
@@ -5,7 +5,14 @@ const app = express(); // 관습적으로 app
 let members = require("./members");
 
 app.get("/api/members", (req, res) => {
-  res.send(members);
+  // res.send(members);
+  const { team } = req.query;
+  if (team) {
+    const teamMembers = members.filter((m) => m.team === team);
+    res.send(teamMembers);
+  } else {
+    res.send(members);
+  }
 }); // 콜백 혹은 route handler라고함
 // req : 이 객체를 통해서 클라이언트가 보낸 request를 다룰수 있음
 // res : 이 객체를 통해서 적절한 리스폰스를 보낼수 있다.
