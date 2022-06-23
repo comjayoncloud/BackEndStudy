cb8c5ae02ca608926e055821d7104c8646ddcce4
jaewoo <comjayoncloud@gmail.com>
2022-06-23 23:47:39 +0900

    미들웨어

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-const users = ["Tom", "Andy", "Jessica", "Paul"]; // const
+const users = ["Tom", "Andy", "Jessica", "Paul", "jaewoo"]; // const
 
 app.get("/", (request, response) => {
   response.end("<h1>Weclome</h1>");
@@ -26,24 +26,3 @@ app.get("*", (request, response) => {
 });
 
 app.listen(3000);
-
-// const users = ["Tom", "Andy", "Jessica", "Paul"]; // const
-
-// const server = http.createServer((request, response) => {
-//   // Arrow Function, const
-//   if (request.url === "/") {
-//     response.end("<h1>Welcome!</h1>");
-//   } else if (request.url === "/users") {
-//     response.end(`<h1>${users}/h1>`); // Template String
-//   } else if (request.url.split("/")[1] === "users") {
-//     // url : /users/1, /users/2, .. // etc
-//     const userIdx = request.url.split("/")[2];
-//     const userName = users[userIdx - 1]; // etc
-
-//     response.end(`<h1>${userName}</h1>`); // Template String
-//   } else {
-//     response.end("<h1>Page Not Available</h1>");
-//   }
-// });
-
-// server.listen(3000);
